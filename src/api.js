cd403a9277b32bc25ebc996b863e09b9efb7b271
Jesse <101078250+JesseProgramming@users.noreply.github.com>
2022-11-27 23:35:59 -0800

    update gitignore

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fdc8858247mshcc1353c62ade90fp10a5c1jsne39c982289c0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a90c31b83b5033302a08d6a42ec3617e";
